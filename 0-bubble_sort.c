1ce439c9940435d6f5928c1370b21ab7813017e2
= <gm38929[at]gmail.com>
2023-01-30 11:06:21 +0000

    Rewrite files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,38 @@
+#include <stddef.h>
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
- */
+  * bubble_sort - sorts an array of integers in ascending order
+  * using bubble sort.
+  * @array: array of integers
+  * @size: size of array
+  */
 void bubble_sort(int *array, size_t size)
 {
-
-	size_t i, index, tmp = 0;
+	size_t i, j;
 
 	if (size < 2)
 		return;
 	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+	{
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[j] > array[j + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
 			}
 		}
+	}
+}
+/**
+  * swap - swaps two elements
+  * @a: first element
+  * @b: second element
+  */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,41 @@
+#include <stddef.h>
 #include "sort.h"
 /**
- * selection_sort - function that sorts an array of integers in ascending
- * order using the Selection sort algorithm
- * @size: size of the array
- * @array: list with numbers
- */
+  * selection_sort - sorts an array of integers in ascending
+  * order using selection sort
+  * @array: array of integers
+  * @size: size of array
+  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, index;
-	int tmp, swap, flag = 0;
+	size_t step, min_idx, i;
 
-	if (array == NULL)
-		return;
-	for (i = 0; i < size; i++)
+	for (step = 0; step < size - 1; step++)
 	{
-		tmp = i;
-		flag = 0;
-		for (index = i + 1; index < size; index++)
+		min_idx = step;
+		for (i = step + 1; i < size; i++)
 		{
-			if (array[tmp] > array[index])
+			if (array[i] < array[min_idx])
 			{
-				tmp = index;
-				flag += 1;
+				min_idx = i;
 			}
 		}
-		swap = array[i];
-		array[i] = array[tmp];
-		array[tmp] = swap;
-		if (flag != 0)
+		if (min_idx != step)
+		{
+			swap(&array[min_idx], &array[step]);
 			print_array(array, size);
+		}
 	}
+}
+
+/**
+  * swap - swaps two elements
+  * @a: first element
+  * @b: second element
+  */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
